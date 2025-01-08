397e7183e52781fa900d9e3d4ea22c3da235dd52
valebdn123 <valentine.budin@oteria.fr>
2025-01-08 19:11:20 +0100

    ecdsa.py

--- ecdsa.py
+++ ecdsa.py
@@ -14,17 +14,35 @@ def H(message):
     h = SHA256.new(message)
     return (int(h.hexdigest(), 16))
 
-def ECDSA_generate_nonce("""TBC"""):
-    return """TBC"""
+def ECDSA_generate_nonce(order):
+    return randint (1, order -1)
 
 
-def ECDSA_generate_keys("""TBC"""):
-    return ("""TBC""")
+def ECDSA_generate_keys(curve):
+    private_key = randint(1, curve.order -1)
+    public_key = curve.G * private_key
+    return private_key, public_key
 
 
-def ECDSA_sign("""TBC"""):
-    return ("""TBC""")    
+def ECDSA_sign(self, z, k):
+    assert 0 < k < self._curve.order
+
+    order = self._curve.order
+    blind = Integer.random_range (min_inclusive=1, max_exclusive=order)
+
+    blind_d = self._d * blind
+    inv_blink_k = (blind * k).inverse(order)
+
+    r = (self._curve.G * k).x % order
+    s = inv_blind_k * (blind * z + blind_d * r) % order
+
+    return (r,s)    
 
 
-def ECDSA_verify("""TBC"""):
-    return """TBC"""
+def ECDSA_verify(self, z, rs):
+    order = self._curve.order
+    sinv = rs [1].inverse(order)
+    point1 = self._curve.G * ((sinv * z) % order)
+    point2 = self.pointQ * ((sinv * rs[0]) % order)
+    return (point1 + point2).x == rs [0]
+    
