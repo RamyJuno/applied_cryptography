aed76a128b909219a36fec689654733c30bee2c6
Ramy <ramy.benmamar@gmail.com>
2025-01-08 19:28:36 +0100

    DSA verify completed

--- test.py
+++ test.py
@@ -10,32 +10,31 @@ PARAM_Q = 0x8CF83642A709A097B447997640129DA299B1A47D1EB3750BA308B0FE64F5FBD3
 
 PARAM_G = 0x3FB32C9B73134D0B2E77506660EDBD484CA7B18F21EF205407F4793A1A0BA12510DBC15077BE463FFF4FED4AAC0BB555BE3A6C1B0C6B47B1BC3773BF7E8C6F62901228F8C28CBB18A55AE31341000A650196F931C77A57F2DDF463E5E9EC144B777DE62AAAB8A8628AC376D282D6ED3864E67982428EBC831D14348F6F2F9193B5045AF2767164E1DFC967C1FB3F2E55A4BD1BFFE83B9C80D052B985D182EA0ADB2A3B7313D3FE14C8484B1E052588B9B7D2BBD2DF016199ECD06E1557CD0915B3353BBB64E0EC377FD028370DF92B52C7891428CDC67EB6184B523D1DB246C32F63078490F00EF8D647D148D47954515E2327CFEF98C582664B4C0F6CC41659
 
+message = b'An important message !'
+
+k = 0x7e7f77278fe5232f30056200582ab6e7cae23992bca75929573b779c62ef4759
+
+x = 0x49582493d17932dabd014bb712fc55af453ebfb2767537007b0ccff6e857e6a3
+
+y = pow(PARAM_G, x, PARAM_P)
+
 def H(message):
     h = SHA256.new(message)
     return (int(h.hexdigest(), 16))
 
-def DSA_generate_nonce(("""TBC""")):
-
-    return ("""TBC""")
-
-
-def DSA_generate_keys("""TBC"""):
-    
-    return """TBC"""
-
-
 def DSA_sign(g, p, q, x, m):
     while True:
             # Generation du nonce
-            k = randrange(2, q)
+            # k = randrange(2, q)
             r = pow(g, k, p) % q
             # SHA_256(message)
             mess = H(m)
             try:
                 s = (mod_inv(k, q) * (mess + x * r)) % q
-                return hex(r), hex(s)
+                return r, s
             except ZeroDivisionError:
                 pass  
+
 
 def DSA_verify(r, s, g, p, q, y, m):
     if not 0 < r < q or not 0 < s < q :
@@ -48,3 +47,9 @@ def DSA_verify(r, s, g, p, q, y, m):
     if v == r:
         return True
     return False
+
+keys = DSA_sign(PARAM_G, PARAM_P, PARAM_Q, x, message)
+
+print(keys)
+
+print(DSA_verify(keys[0], keys[1], PARAM_G, PARAM_P, PARAM_Q, y, message))
